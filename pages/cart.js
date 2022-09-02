3bec8294280a582e5ffa8da8389a66efcb1be1f6
haikalrozhan <haikalrozhan1992@yahoo.com>
2022-09-02 09:14:39 +0800

    save cart items

--- pages/cart.js
+++ pages/cart.js
@@ -5,6 +5,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { XCircleIcon } from "@heroicons/react/24/outline";
 import { useRouter } from "next/router";
+import dynamic from "next/dynamic";
 
 const CartScreen = () => {
   const { state, dispatch } = useContext(Store);
@@ -106,4 +107,4 @@ const CartScreen = () => {
   );
 };
 
-export default CartScreen;
+export default dynamic(() => Promise.resolve(CartScreen), { ssr: false });
